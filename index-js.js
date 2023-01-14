26e6bd257e42ef12fbaf9bee739240713883447c
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-01-14 18:52:33 +0800

    aaa

--- index-js.js
+++ index-js.js
@@ -1,9 +1,9 @@
 function barc(){
     alert('點"確定"');
-    location.href() == 'https://twolucasplaystdios.github.io/bogoarchive.github.io/';
+    onclick = location.href('https://twolucasplaystdios.github.io/bogoarchive.github.io/');
 }
 
 function homewk(){
     alert('點"確定"');
-    location.href() == 'https://twolucasplaystdios.github.io/twolucasplaystdios-homework.github.io/';
+    onclick == location.href('https://twolucasplaystdios.github.io/twolucasplaystdios-homework.github.io/');
 }
