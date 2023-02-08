a189ba18bc40d634a57996946d58b18f488524a0
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-02-08 10:33:35 +0000

    aaxaxax

--- index-js.js
+++ index-js.js
@@ -10,8 +10,3 @@ function homewk(){
     location.href = "https://twolucasplaystdios.github.io/twolucasplaystdios-homework.github.io/";
 }
 
-function shre(){
-    let url = location.href;
-    alert('點\"確定\"');
-    location.href = "blog/index.html";
-}
