af58b8d1e76aa16fb7d9d07fabc06d58f8721f46
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-03-10 21:18:10 +0800

    aaaaaaaaaaaa

---
+++ posts/0/index-js.js
@@ -0,0 +1,12 @@
+function barc(){
+    let url = location.href;
+    alert('點\"確定\"');
+    location.href = "https://twolucasplaystdios.github.io/bogoarchive.github.io/"
+}
+
+function homewk(){
+    let url = location.href;
+    alert('點\"確定\"');
+    location.href = "https://twolucasplaystdios.github.io/twolucasplaystdios-homework.github.io/";
+}
+
